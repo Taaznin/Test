16af29c5a52ee843f0c98542249983c498725bed
Taaznin <taazahan@gmail.com>
2020-02-12 12:01:58 +0600

    information

---
+++ resources/views/student.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>student information</title>
+</head>
+<body>
+	<form>
+  <label>First name:<br></label>
+  <input type="text" name="firstname">
+  <br>
+  <label>Last name:<br></label>
+  <input type="text" name="lastname">
+  <br>
+  <label>Roll:<br></label>
+  <input type="text" name="roll">
+  <br>
+  <label>Present Address:<br></label>
+  <input type="text" name="present_address">
+  <br>
+  <label>Parmanent Address:<br></label>
+  <input type="text" name="parmanent_address">
+  <br>
+  <label>Phone:<br></label>
+  <input type="text" name="phone">
+  <br><br>
+
+  <button type="submit" name="submit" class="btn btn-success">Submit</button>
+</form>
+
+</body>
+</html>
